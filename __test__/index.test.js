e210531d181f1ebc401cfe54d66ff2cb38bdead5
Ivan Opara <oiv84@icloud.com>
2022-01-24 23:01:31 +0300

    Added JSON plain test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,16 +1,19 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 
-const filepath1 = '__fixtures__/file1.json';
-const filepath2 = '__fixtures__/file2.json';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('genDiff JSON plain', () => {
-  expect(genDiff(filepath1, filepath2))
-    .toEqual(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`);
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+
+  const result = readFile('result.txt');
+
+  expect(genDiff(filepath1, filepath2)).toEqual(result);
 });
