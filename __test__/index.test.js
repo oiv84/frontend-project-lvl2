d90b70dcdd43463730b0a327959524a906efa8d2
Ivan Opara <oiv84@icloud.com>
2022-01-26 22:24:17 +0300

    Added test for plain YAML files

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -17,3 +17,12 @@ test('genDiff JSON plain', () => {
 
   expect(genDiff(filepath1, filepath2)).toEqual(result);
 });
+
+test('genDiff YAML plain', () => {
+  const filepath1 = getFixturePath('file1.yml');
+  const filepath2 = getFixturePath('file2.yml');
+
+  const result = readFile('result.txt');
+
+  expect(genDiff(filepath1, filepath2)).toEqual(result);
+});
