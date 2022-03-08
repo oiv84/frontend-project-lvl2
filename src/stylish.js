1da2d129118c0500193d727a5d4bcf2420122e15
Ivan Opara <oiv84@icloud.com>
2022-03-08 18:48:13 +0300

    Added logic for complex files

--- src/stylish.js
+++ src/stylish.js
@@ -1,23 +1,34 @@
-const stylish = (ast, indentSize = 2) => {
-  const indent = (' ').repeat(indentSize);
+import _ from 'lodash';
 
-  const diff = ast.map((item) => {
-    if (item.status === 'removed') {
-      return `${indent}- ${item.key}: ${item.value}\n`;
-    }
+const indent = (spaces) => ('    '.repeat(spaces));
 
-    if (item.status === 'added') {
-      return `${indent}+ ${item.key}: ${item.value}\n`;
-    }
-
-    if (item.status === 'changed') {
-      return `${indent}- ${item.key}: ${item.oldValue}\n${indent}+ ${item.key}: ${item.newValue}\n`;
-    }
-
-    return `${indent}${indent}${item.key}: ${item.value}\n`;
-  });
-
-  return `{\n${diff.join('')}}`;
+const stringify = (value, spaces = 0) => {
+  if (!_.isObject(value)) {
+    return value;
+  }
+  const lines = _.keys(value).map((node) => `${indent(spaces)}    ${node}: ${stringify(value[node], spaces + 1)}`);
+  const innerValue = lines.join('\n');
+  return `{\n${innerValue}\n${indent(spaces)}}`;
 };
 
-export default (ast) => stylish(ast, 2);
+const stylish = (ast, spaces = 0) => {
+  const lines = ast.map((node) => {
+    const buildLine = (char, value) => `${indent(spaces)}  ${char} ${node.key}: ${stringify(value, spaces + 1)}`;
+    switch (node.status) {
+      case 'removed':
+        return buildLine('-', node.value);
+      case 'unchanged':
+        return buildLine(' ', node.value);
+      case 'changed':
+        return `${indent(spaces)}  - ${node.key}: ${stringify(node.oldValue, spaces + 1)}\n ${indent(spaces)} + ${node.key}: ${stringify(node.newValue, spaces + 1)}`;
+      case 'added':
+        return buildLine('+', node.value);
+      default:
+        return `${indent(spaces)}    ${node.key}: ${stylish(node.value, spaces + 1)}`;
+    }
+  });
+  const innerValue = lines.join('\n');
+  return `{\n${innerValue}\n${indent(spaces)}}`;
+};
+
+export default stylish;
