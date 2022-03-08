351cf5873c765f7bebc44f06b83f83187ff105a3
Ivan Opara <oiv84@icloud.com>
2022-03-08 18:49:15 +0300

    Added stylish formatter as default

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,19 +1,16 @@
 #!/usr/bin/env node
-
 import { Command } from 'commander';
-
 import genDiff from '../src/index.js';
 
 const program = new Command();
-
 program
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+    const { format } = program.opts();
+    const diff = genDiff(filepath1, filepath2, format);
+    console.log(diff);
   });
-
-program.parse();
+program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import stylish from './stylish.js';
 import parse from './parsers.js';
 import buildAst from './astBuilder.js';
 
-const compareTwoFiles = (filepath1, filepath2) => {
+const compareTwoFiles = (filepath1, filepath2, format = 'stylish') => {
   const data1 = parse(path.resolve(process.cwd(), filepath1));
   const data2 = parse(path.resolve(process.cwd(), filepath2));
 
-  return stylish(buildAst(data1, data2));
+  const ast = buildAst(data1, data2);
+
+  const render = {
+    stylish,
+  };
+
+  return render[format](ast);
 };
 
 export default (compareTwoFiles);
