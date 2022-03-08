3e447c798d307d31c6a2675970671ba9b717bdcd
Ivan Opara <oiv84@icloud.com>
2022-03-08 16:06:10 +0300

    Added astBuilder

---
+++ src/astBuilder.js
@@ -0,0 +1,42 @@
+import _ from 'lodash';
+
+const buildAst = (data1, data2) => {
+  const keys = _.orderBy(_.union(Object.keys(data1), Object.keys(data2)));
+
+  const ast = keys.map((key) => {
+    if (!_.has(data1, key) && _.has(data2, key)) {
+      return {
+        key,
+        status: 'added',
+        value: data2[key],
+      };
+    }
+
+    if (_.has(data1, key) && !_.has(data2, key)) {
+      return {
+        key,
+        status: 'removed',
+        value: data1[key],
+      };
+    }
+
+    if (_.has(data1, key) && _.has(data2, key) && data1[key] !== data2[key]) {
+      return {
+        key,
+        status: 'changed',
+        oldValue: data1[key],
+        newValue: data2[key],
+      };
+    }
+
+    return {
+      key,
+      status: 'unchanged',
+      value: data1[key],
+    };
+  });
+
+  return ast;
+};
+
+export default buildAst;
