948481d2a3f30875bf7662d9b7a23095b406a224
Ivan Opara <oiv84@icloud.com>
2022-03-09 01:08:48 +0300

    Refactored parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -2,17 +2,21 @@ import yaml from 'js-yaml';
 import fs from 'fs';
 import path from 'path';
 
-const parseFile = (filePath) => {
+const parseMapping = {
+  '.json': JSON.parse,
+  '.yaml': yaml.load,
+  '.yml': yaml.load,
+};
+
+export default (filePath) => {
   const file = fs.readFileSync(filePath, 'utf8');
   const fileExt = path.extname(filePath);
 
-  const parseEngine = {
-    '.json': JSON.parse,
-    '.yaml': yaml.load,
-    '.yml': yaml.load,
-  };
+  const parse = parseMapping[fileExt];
 
-  return parseEngine[fileExt](file);
+  if (!parse) {
+    throw new Error(`There is now parser for "${fileExt}" files`);
+  }
+
+  return parse(file);
 };
-
-export default parseFile;
