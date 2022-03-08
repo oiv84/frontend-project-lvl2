31a55d583240270797514369145fdecf62c4ccb4
Ivan Opara <oiv84@icloud.com>
2022-03-08 16:07:11 +0300

    Added stylish for ast

--- src/index.js
+++ src/index.js
@@ -1,29 +1,14 @@
 import path from 'path';
-import _ from 'lodash';
+
+import stylish from './stylish.js';
 import parse from './parsers.js';
+import buildAst from './astBuilder.js';
 
-const compaireTwoFiles = (filepath1, filepath2) => {
-  const file1 = parse(path.resolve(process.cwd(), filepath1));
-  const file2 = parse(path.resolve(process.cwd(), filepath2));
+const compareTwoFiles = (filepath1, filepath2) => {
+  const data1 = parse(path.resolve(process.cwd(), filepath1));
+  const data2 = parse(path.resolve(process.cwd(), filepath2));
 
-  const keys = _.orderBy(_.union(Object.keys(file1), Object.keys(file2)));
-
-  const compare = (key) => {
-    if (_.has(file1, key)) {
-      if (_.has(file2, key)) {
-        if (file1[key] === file2[key]) {
-          return `    ${key}: ${file1[key]}\n`;
-        }
-        return `  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}\n`;
-      }
-      return `  - ${key}: ${file1[key]}\n`;
-    }
-    return `  + ${key}: ${file2[key]}\n`;
-  };
-
-  const diff = keys.map((key) => compare(key));
-
-  return `{\n${diff.join('')}}`;
+  return stylish(buildAst(data1, data2));
 };
 
-export default compaireTwoFiles;
+export default (compareTwoFiles);
