5fa66a2cf5d3f5df5c6f02f7ae38ed912afe9de6
Ivan Opara <oiv84@icloud.com>
2022-03-10 13:47:01 +0300

    Refactored astBuilder

--- src/astBuilder.js
+++ src/astBuilder.js
@@ -1,48 +1,36 @@
 import _ from 'lodash';
 
-const buildNode = (data1, data2) => {
+const sortedKeys = (data1, data2) => {
   const keys = _.union(_.keys(data1), _.keys(data2));
-  const sortedKeys = _.sortBy(keys);
+  return _.sortBy(keys);
+};
 
-  const ast = sortedKeys.map((key) => {
-    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+const buildNode = (data1, data2) => {
+  const isObject = (key) => _.isPlainObject(data1[key]) && _.isPlainObject(data2[key]);
+  const isAdded = (key) => !_.has(data1, key) && _.has(data2, key);
+  const isRemoved = (key) => _.has(data1, key) && !_.has(data2, key);
+  const isChanged = (key) => !_.isEqual(data1[key], data2[key]);
+
+  const ast = sortedKeys(data1, data2).map((key) => {
+    if (isObject(key)) {
+      return { key, type: 'object', children: buildNode(data1[key], data2[key]) };
+    }
+
+    if (isAdded(key)) {
+      return { key, type: 'added', value: data2[key] };
+    }
+
+    if (isRemoved(key)) {
+      return { key, type: 'removed', value: data1[key] };
+    }
+
+    if (isChanged(key)) {
       return {
-        key,
-        type: 'object',
-        children: buildNode(data1[key], data2[key]),
+        key, type: 'changed', oldValue: data1[key], newValue: data2[key],
       };
     }
 
-    if (!_.has(data1, key) && _.has(data2, key)) {
-      return {
-        key,
-        type: 'added',
-        value: data2[key],
-      };
-    }
-
-    if (_.has(data1, key) && !_.has(data2, key)) {
-      return {
-        key,
-        type: 'removed',
-        value: data1[key],
-      };
-    }
-
-    if (!_.isEqual(data1[key], data2[key])) {
-      return {
-        key,
-        type: 'changed',
-        oldValue: data1[key],
-        newValue: data2[key],
-      };
-    }
-
-    return {
-      key,
-      type: 'unchanged',
-      value: data2[key],
-    };
+    return { key, type: 'unchanged', value: data2[key] };
   });
 
   return ast;
