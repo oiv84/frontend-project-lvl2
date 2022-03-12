3028975fad314dc8e67f6082032fc6bd1ced3100
Ivan Opara <oiv84@icloud.com>
2022-03-12 12:57:07 +0300

    Replaced filereading logic from parsers.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import path from 'path';
+import fs from 'fs';
 
 import format from './formatters/index.js';
 import parse from './parsers.js';
 import buildAst from './astBuilder.js';
 
-const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
+const getFullPath = (filePath) => path.resolve(process.cwd(), filePath);
+const getFile = (filePath) => fs.readFileSync(getFullPath(filePath), 'utf8');
+const getFormat = (filePath) => path.extname(filePath).replace('.', '');
 
-const genDiff = (filepath1, filepath2, outputFormat = 'stylish') => {
-  const data1 = parse(getFullPath(filepath1));
-  const data2 = parse(getFullPath(filepath2));
+const genDiff = (filePath1, filePath2, outputFormat = 'stylish') => {
+  const data1 = parse(getFile(filePath1), getFormat(filePath1));
+  const data2 = parse(getFile(filePath2), getFormat(filePath2));
 
   const ast = buildAst(data1, data2);
 
--- src/parsers.js
+++ src/parsers.js
@@ -1,21 +1,16 @@
 import yaml from 'js-yaml';
-import fs from 'fs';
-import path from 'path';
 
 const parseMapping = {
-  '.json': JSON.parse,
-  '.yaml': yaml.load,
-  '.yml': yaml.load,
+  json: JSON.parse,
+  yaml: yaml.load,
+  yml: yaml.load,
 };
 
-export default (filePath) => {
-  const file = fs.readFileSync(filePath, 'utf8');
-  const fileExt = path.extname(filePath);
-
-  const parse = parseMapping[fileExt];
+export default (file, format) => {
+  const parse = parseMapping[format];
 
   if (!parse) {
-    throw new Error(`There is now parser for "${fileExt}" files`);
+    throw new Error(`There is now parser for "${format}" files`);
   }
 
   return parse(file);
